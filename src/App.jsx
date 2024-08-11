6d6b43ea7c6a254f1a90e1ef49c99074aebd19a4
Tarun45GitHub <td2545@it.jgec.ac.in>
2024-08-11 11:37:50 +0530

    redux toolkit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appWrite/auth'
+import { login,logout } from './Store/AuthSlice'
 import conf from './conf/conf'
+import {Header,Footer} from "./components/index"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-console.log(conf.AppWriteDatabaseId)
-  return (
-   <h1>Blog Writting App</h1>
-   
-  )
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+  .then((userData)=>{
+    if(userData) dispatch(login({userData}));
+    else dispatch(logout());
+  })
+    .finally(()=>setLoading(false))
+  },[])
+
+ 
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          wdfghj
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
